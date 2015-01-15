171755af6e613303560e9e70b16200624f028b0f
Fidel Tamondong <fidel.tamondong@gmail.com>
2015-01-15 21:03:29 +0800

    Adds option in remove_characters.py to remove characters starting from the left or right

--- scripts/remove_characters.py
+++ scripts/remove_characters.py
@@ -15,7 +15,7 @@ except ImportError:
 def init(jabr, update_newname):
   init.rename = 1
   init.update_newname = update_newname
-  beg_label = tkinter.Label(jabr, text='Remove From:')
+  beg_label = tkinter.Label(jabr, text='Start From:')
   beg_label.grid(column=0, row=0, sticky='e')
   init.beg_spinbox = tkinter.Spinbox(jabr, from_=0, to=9999, command=check_inputs)
   init.beg_spinbox.grid(column=1, row=0, sticky='ew')
@@ -27,7 +27,7 @@ def init(jabr, update_newname):
   init.end_spinbox.grid(column=1, row=1, sticky='ew')
   init.end_spinbox.bind("<KeyRelease>", check_inputs)
   
-  """init.from_options = ['From the Left', 'From the Right']
+  init.from_options = ['From the Left', 'From the Right']
   init.fromvar = tkinter.StringVar()
   init.fromvar.set(init.from_options[0])
   from_optionmenu = tkinter.OptionMenu(jabr,
@@ -35,7 +35,7 @@ def init(jabr, update_newname):
                                        *init.from_options,
                                        command=update_filename)
   from_optionmenu.configure(width=15)
-  from_optionmenu.grid(column=1, row=2, sticky='ew')"""
+  from_optionmenu.grid(column=1, row=2, sticky='ew')
   
   jabr.grid_columnconfigure(1, weight=1)
   
@@ -51,8 +51,16 @@ def check_inputs(key=''):
   init.update_newname()
     
 def update_filename(string):
+  from_where = init.fromvar.get()
+  
   if init.rename == 0:
     return
   from_num = int(init.beg_spinbox.get())
   to_num = int(init.end_spinbox.get())
-  return string[:from_num] + string[to_num:]
+  
+  if from_num < to_num:
+    if from_where == init.from_options[0]:
+      return string[:from_num] + string[to_num:]
+    else:
+      from_num = -(len(string)) if (from_num == 0) else from_num
+      return string[:-(to_num)] + string[-(from_num):]
